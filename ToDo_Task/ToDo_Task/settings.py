1f1d1333e07451fb39242adea45a4ba6f0b4e971
Ishrakul-Tahmid <ishrakultahmid@gmail.com>
2025-05-25 22:55:16 +0600

    Updated

--- ToDo_Task/ToDo_Task/settings.py
+++ ToDo_Task/ToDo_Task/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,11 +74,18 @@ WSGI_APPLICATION = 'ToDo_Task.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://mamar_bank_61ld_user:8dkypVXckpa0DPo3lrgKTdGkdtqpcSpa@dpg-d0ojo5re5dus73cudfc0-a.oregon-postgres.render.com/mamar_bank_61ld',
+        conn_max_age=600
+    )
 }
 
 
